0e6f1990be221e56337d7fd275ad74f32c02f94b
Hardcorelevelingwarrior <nhuducminh123@gmail.com>
2023-08-19 12:52:43 +0700

    ADD A LAST_FETCHED_AT COLUMN TO THE FEEDS TABLE

--- main.go
+++ main.go
@@ -158,6 +158,15 @@ type request struct {
 	URL string `json:"url,omitempty"`
 	Feed_id uuid.UUID `json:"feed_id,omitempty"`
 }
+type Feed struct {
+    ID            uuid.UUID  `json:"id"`
+    CreatedAt     time.Time  `json:"created_at"`
+    UpdatedAt     time.Time  `json:"updated_at"`
+    Name          string     `json:"name"`
+    URL           string     `json:"url"`
+    UserID        uuid.UUID  `json:"user_id"`
+    LastFetchedAt *time.Time `json:"last_fetched_at,omitempty"`
+}
 
 
 type authedHandler func(http.ResponseWriter, *http.Request, database.User)
@@ -173,4 +182,20 @@ func(cfg *apiConfig) middlewareAuth(handle authedHandler) http.HandlerFunc{
 		if err != nil {respondWithError(w,404,err.Error());return}
 		handle(w,r,users)
 	}
+}
+
+func databaseFeedtoFeed(feed database.Feed) Feed {
+	var lastFetchedAt *time.Time
+    if feed.LastFetchedAt.Valid {
+        lastFetchedAt = &feed.LastFetchedAt.Time
+    }
+    return Feed{
+        ID:            feed.ID,
+        CreatedAt:     feed.CreatedAt,
+        UpdatedAt:     feed.UpdatedAt,
+        Name:          feed.Name,
+        URL:           feed.Url,
+        UserID:        feed.UserID,
+        LastFetchedAt: lastFetchedAt,
+    }
 }
